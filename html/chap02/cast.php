b2545b399be19f6595557718265e1e9833f79685
Soh1121 <satou.shg@gmail.com>
2020-06-28 23:52:01 +0900

    [add]明示的な変換（キャスト）の学習

---
+++ html/chap02/cast.php
@@ -0,0 +1,9 @@
+<?php
+var_dump((int)1530.95);   // 結果：int(1530)
+var_dump((int)-1530.95);  // 結果：int(-1530)
+var_dump((int)true);      // 結果：int(1)
+var_dump((string)true);   // 結果：string(1) "1"
+var_dump((int)false);     // 結果：int(0)
+var_dump((string)false);  // 結果：string(0) ""
+var_dump((array)108);     // 結果：array(1) { [0]=>int(108) }
+var_dump((unset)1530.95); // 結果：NULL
